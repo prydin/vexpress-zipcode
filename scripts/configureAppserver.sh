ec77b389cd104c83105ddbf02c064532027394bb
prydin <prydin@vmware.com>
2020-12-23 14:16:50 -0500

    WIP

--- scripts/configureAppserver.sh
+++ scripts/configureAppserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /opt/vexpress-zipcode
 chown $1 vexpress-zipcode
-cd vexpress-zipcode
+cd /opt/vexpress-zipcode
 mv /tmp/application.properties .
 wget --auth-no-challenge --user=$2 --password=$3 $4/artifact/build/libs/zipcode-$5.jar -o zipcode.jar
 mv /tmp/vexpress-zipcode.service /etc/systemd/system

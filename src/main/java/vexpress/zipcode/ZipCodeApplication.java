b66ea70e281d35ad44aa5543208ff7c3bf2fa2a8
prydin <prydin@vmware.com>
2021-01-04 11:19:01 -0500

    Updated springboot

--- src/main/java/vexpress/zipcode/ZipCodeApplication.java
+++ src/main/java/vexpress/zipcode/ZipCodeApplication.java
@@ -2,8 +2,10 @@ package vexpress.zipcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class ZipCodeApplication {
   public static void main(final String[] args) {
     SpringApplication.run(ZipCodeApplication.class, args);
